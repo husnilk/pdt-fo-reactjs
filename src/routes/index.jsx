b6b1f846a73184f5e59a41701233f619f153d872
Husnil Kamil <husnilk@it.unand.ac.id>
2025-02-11 17:35:56 +0700

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "../views/home/index";
+import Login from "../views/auth/login";
+import Register from "../views/auth/register";
+import Dashboard from "../views/admin/dashboard";
+
+export default function AppRoutes() {
+  const { isAuthenticated } = useContext(AuthContext);
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/register"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/admin/dashboard" replace />
+          ) : (
+            <Register />
+          )
+        }
+      />
+
+      <Route
+        path="/login"
+        element={
+          isAuthenticated ? (
+            <Navigate to="/admin/dashboard" replace />
+          ) : (
+            <Login />
+          )
+        }
+      />
+
+      <Route
+        path="/admin/dashboard"
+        element={
+          isAuthenticated ? <Dashboard /> : <Navigate to="/login" replace />
+        }
+      />
+    </Routes>
+  );
+}
